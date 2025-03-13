0cd00265d6ab9be71e29c530bbe1dc4e9fa83e70
JosueMatamoros <1002matamoros@gmail.com>
2025-03-13 15:08:20 -0600

    .

--- SolucionesElectricas/src/pages/HomePage.jsx
+++ SolucionesElectricas/src/pages/HomePage.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { useTheme } from '../context/ThemeContext'
+import React from "react";
+import { useTheme } from "../context/ThemeContext";
 
 export default function HomePage() {
-  const { isDarkMode } = useTheme()
+  const { isDarkMode } = useTheme();
 
   return (
-
     <div
-    className={`h-screen w-full transition-all duration-500 ${
-      isDarkMode ? 'bg-main-dark-bg' : ' bg-main-light-bg'
-    }`}>
-      
-
+      className={`h-screen w-full transition-all duration-500 ${
+        isDarkMode ? "bg-main-dark-bg" : " bg-main-light-bg"
+      }`}
+    >
+      Grupo sen
     </div>
-  )
+  );
 }
